608e735775cacd3f985b02ad1f87612e88dc59f1
vosterss <77280391+vosterss@users.noreply.github.com>
2021-01-11 16:19:42 +0100

    commit main
    Co-Authored-By: LorisTrr <77280327+LorisTrr@users.noreply.github.com>
    Co-Authored-By: ggiraudet <77280389+ggiraudet@users.noreply.github.com>

--- view/main.php
+++ view/main.php
@@ -1,3 +1,32 @@
 <main>
-	
+  	<div class="navigation">
+		<div class="bouton-resume">
+			<a id="lien-R" href="index.html"> 
+	    		<button class="bouton-R" type="button">
+	            	<h2> Acceuil </h2>
+	    		</button>
+			</a>
+		</div>
+	    <div class="bouton-personnage">
+	    	<a id="lien-P" href="character.html">
+	    		<button class="bouton-P" type="button">		                		
+	            	<h2> Profil </h2>
+	    		</button>
+			</a>
+	    </div>
+	    <div class="bouton-lieux">
+			<a id="lien-L" href="lieux.html">
+	    		<button class="bouton-L" type="button">		                		
+	            	<h2> Forum </h2>
+	    		</button>
+			</a>			   
+	    </div>
+	    <div class="bouton-quiz">
+	    	<a id="lien-Q" href="quizz.html"> 
+	    		<button class="bouton-Q" type="button">		                		
+	            	<h2> Quizz </h2>
+	    		</button>
+			</a>
+	    </div>
+	</div>	
 </main>
