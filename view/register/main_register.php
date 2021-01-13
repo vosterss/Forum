585d7696bf0edc2213bc7bb8ede83dcae682d370
vosterss <77280391+vosterss@users.noreply.github.com>
2021-01-13 19:52:14 +0100

    Update main_register.php

--- view/register/main_register.php
+++ view/register/main_register.php
@@ -5,11 +5,34 @@
 		</div>
 		<div class="form">
 			<form action="action_inscription.php" method="post">
-				<p> Prénom : <input type="text" name="surname" required minlength="3" maxlength="30"/></p>
-				<p> Nom : <input type="text" name="name" required minlength="3" maxlength="30"/></p>
-				<p> Nom d'utilisateur : <input type="text" name="username" required minlength="3" maxlength="30"/></p>
-				<p>Mail : <input type="email" name="mail"/></p>
-				<p> Mot de passe : <input type="text" name="password" required minlength="3" maxlength="30"/></p>
+				<p> 
+					<label> Prénom </label> 
+					<input type="text" name="surname" required minlength="3" maxlength="30"/>
+				</p>
+				<p> 
+					<label> Nom </label>
+					<input type="text" name="name" required minlength="3" maxlength="30"/>
+				</p>
+				<p> 
+					<label> Nom d'utilisateur </label>
+					<input type="text" name="username" required minlength="3" maxlength="30"/>
+				</p>
+				<p>
+				 	<label> Mail </label>
+				 	<input type="email" name="mail"/>
+				</p>
+				<p> 
+					<label> Mot de passe (8 caractères minimum) </label>
+					<input type="text" name="password" required minlength="8"/>
+				</p>
+				<p> 
+					<label> Date de naissance </label>
+					<input type="date" name="birthday"/>
+				</p>
+				<p>
+					<input type="submit" value="Valider"/>
+				</p>
+			</form>
 		</div>	
 	</div>
 </main>
