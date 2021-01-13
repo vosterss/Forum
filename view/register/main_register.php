4b7a9804b18e23b037a23f18a5fadb67b3f20bb8
vosterss <77280391+vosterss@users.noreply.github.com>
2021-01-13 21:49:59 +0100

    fe

--- view/register/main_register.php
+++ view/register/main_register.php
@@ -6,30 +6,30 @@
 		<div class="form">
 			<form action="action_inscription.php" method="post">
 				<p> 
-					<input type="text" name="surname" placeholder="Prénom" required minlength="3" maxlength="30"/>
+					<input class="form_input" type="text" name="surname" placeholder="Prénom" required minlength="3" maxlength="30"/>
 				</p>
 				<p> 
-					<input type="text" name="name" placeholder="Nom" required minlength="3" maxlength="30"/>
+					<input class="form_input" type="text" name="name" placeholder="Nom" required minlength="3" maxlength="30"/>
 				</p>
 				<p> 
-					<input type="text" name="username" placeholder="Nom d'utilisateur" required minlength="3" maxlength="30"/>
+					<input class="form_input" type="text" name="username" placeholder="Nom d'utilisateur" required minlength="3" maxlength="30"/>
 				</p>
 				<p>
-				 	<input type="email" name="mail" placeholder="Adresse email"/>
+				 	<input class="form_input" type="email" name="mail" placeholder="Adresse email"/>
 				</p>
 				<p> 
-					<input type="password" name="password" placeholder="Mot de passe" required minlength="8"/>
+					<input class="form_input" type="password" name="password" placeholder="Mot de passe" required minlength="8"/>
 				</p>
 				<div class="form_birthday">
 					<div class="form_birthday_titre">
 						<label> Date de naissance </label>
 					</div>
 					<div class="form_birthday_date">
-						<input type="date" name="birthday"/>
+						<input class="form_input" type="date" name="birthday"/>
 					</div>	
 				</div>
 				<div class="form_submit">
-					<input type="submit" value="S'inscrire"/>
+					<input class="form_input" type="submit" value="S'inscrire"/>
 				</div>
 			</form>
 		</div>	
