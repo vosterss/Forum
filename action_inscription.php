4f6fff328cddce4837ad0b2ffb5056338c9f46d2
vosterss <77280391+vosterss@users.noreply.github.com>
2021-01-13 22:44:17 +0100

    fe

--- action_inscription.php
+++ action_inscription.php
@@ -23,47 +23,19 @@
 					$mdp = md5 ($_POST['password']);
 					$birthday = htmlspecialchars($_POST['birthday']);
 
-					if (empty($surname)){
-						echo "Le Prenom est obligatoire.";
-						$erreur ++;
-					}
-					else if (empty($name)){
-						echo "Le Nom de famille est obligatoire.";
-						$erreur ++;
-					}
-					else if (empty($username)){
-						echo "Le nom d'usage est obligatoire.";
-						$erreur ++;
-					}
-					else if (empty($mail)){
-						echo "Le mail est obligatoire.";
-						$erreur ++;
-					}
-					else if (empty($mdp)){
-						echo "Le mot de passe est obligatoire.";
-						$erreur ++;
-					}
-					else if (empty($birthday)){
-						echo "La Date de naissance est obligatoire.";
-						$erreur ++;
-					}
 					$dsn='mysql:host=localhost;dbname=forum';
 					$user='root' ;
-					$password='' ;
-					if($erreur < 0){
-						try{
-							$bdd = new PDO($dsn, $user, $password);
-						}
-						catch(PDOException $e){
-							die ('erreur :' .$e->getMessage());
-						}
-						$reponse=$bdd->prepare('INSERT INTO utilisateur(surname,name,username,mail,password,birthday) VALUES (?,?,?,?,?,?)') ;
-						$reponse->execute(array($surname,$name,$username,$mail,$mdp,$birthday));
-						echo '<p class="test">Bonjour</p> '.$surname. ' '. $name.' vous êtes désormais inscrit';
+					$password='root' ;
+					
+					try{
+						$bdd = new PDO($dsn, $user, $password);
 					}
-					else{
-						echo "Des champs n'ont pas été saisis";
+					catch(PDOException $e){
+						die ('erreur :' .$e->getMessage());
 					}
+					$reponse=$bdd->prepare('INSERT INTO utilisateur(surname,name,username,mail,password,birthday) VALUES (?,?,?,?,?,?)') ;
+					$reponse->execute(array($surname,$name,$username,$mail,$mdp,$birthday));
+					echo '<p class="test">Bonjour</p> '.$surname. ' '. $name.' vous êtes désormais inscrit';
 				?>
 				<a href="index.php">
 					<button class="form_input">
---
+++ login.php
@@ -0,0 +1,5 @@
+<?php
+	require 'view/register/header_register.php';
+	require 'view/register/main_login.php';
+	require 'view/register/footer_register.php';
+?>
