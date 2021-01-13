d13e94b56ca0e7c3da0f4a31e69af641ccbd7b06
LorisTrr <loristerry63430@gmail.com>
2021-01-13 21:37:30 +0100

    vsdvs

--- action_inscription.php
+++ action_inscription.php
@@ -2,56 +2,63 @@
 <html lang="fr">
 	<head>
 		<meta charset="utf-8" />
-		<link rel="stylesheet" type="text/css" href="CSS/style_register.css" media="all"/>
+		<link rel="stylesheet" type="text/css" href="CSS/style_action_inscription.css" media="all"/>
 		<title> Register </title>
 	</head>
 	<body>
-		<h1 class="titre">Félicitation</h1>
 		<?php
-
-			$surname = htmlspecialchars($_POST['surname']);
-			$name = htmlspecialchars($_POST['name']);
-			$username = htmlspecialchars($_POST['username']);
-			$mail = htmlspecialchars($_POST['mail']);
-			$mdp = md5 ($_POST['password']);
-			$birthday = htmlspecialchars($_POST['birthday']);
-
-			if (empty($surname)){
-				echo "Le Prenom est obligatoire.";
-			}
-			else if (empty($name)){
-				echo "Le Nom de famille est obligatoire.";
-			}
-			else if (empty($username)){
-				echo "Le nom d'usage est obligatoire.";
-			}
-			else if (empty($mail)){
-				echo "Le mail est obligatoire.";
-			}
-			else if (empty($mdp)){
-				echo "Le mot de passe est obligatoire.";
-			}
-			else if (empty($birthday)){
-				echo "La Date de naissance est obligatoire.";
-			}
-			$dsn='mysql:host=localhost;dbname=forum';
-			$user='root' ;
-			$password='' ;
-			try{
-				$bdd = new PDO($dsn, $user, $password);
-			}
-			catch(PDOException $e){
-				die ('erreur :' .$e->getMessage());
-			}
-			$reponse=$bdd->prepare('INSERT INTO utilisateur(surname,name,username,mail,password,birthday) VALUES (?,?,?,?,?,?)') ;
-			$reponse->execute(array($surname,$name,$username,$mail,$mdp,$birthday));
-			echo '<p class="test">Bonjour</p> '.$surname. ' '. $name.' vous êtes désormais inscrit';
-
+			require 'view/register/header_register.php';
 		?>
-		<a href="index.php">
-			<button>
-				Retourner à l'accueil
-			</button>
-		</a>
+		<main>
+			<div class="titre_register">
+				<h1 class="titre">Félicitation</h1>
+			</div>
+			<?php
+
+				$surname = htmlspecialchars($_POST['surname']);
+				$name = htmlspecialchars($_POST['name']);
+				$username = htmlspecialchars($_POST['username']);
+				$mail = htmlspecialchars($_POST['mail']);
+				$mdp = md5 ($_POST['password']);
+				$birthday = htmlspecialchars($_POST['birthday']);
+
+				if (empty($surname)){
+					echo "Le Prenom est obligatoire.";
+				}
+				else if (empty($name)){
+					echo "Le Nom de famille est obligatoire.";
+				}
+				else if (empty($username)){
+					echo "Le nom d'usage est obligatoire.";
+				}
+				else if (empty($mail)){
+					echo "Le mail est obligatoire.";
+				}
+				else if (empty($mdp)){
+					echo "Le mot de passe est obligatoire.";
+				}
+				else if (empty($birthday)){
+					echo "La Date de naissance est obligatoire.";
+				}
+				$dsn='mysql:host=localhost;dbname=forum';
+				$user='root' ;
+				$password='' ;
+				try{
+					$bdd = new PDO($dsn, $user, $password);
+				}
+				catch(PDOException $e){
+					die ('erreur :' .$e->getMessage());
+				}
+				$reponse=$bdd->prepare('INSERT INTO utilisateur(surname,name,username,mail,password,birthday) VALUES (?,?,?,?,?,?)') ;
+				$reponse->execute(array($surname,$name,$username,$mail,$mdp,$birthday));
+				echo '<p class="test">Bonjour</p> '.$surname. ' '. $name.' vous êtes désormais inscrit';
+
+			?>
+			<a href="index.php">
+				<button>
+					Retourner à l'accueil
+				</button>
+			</a>
+		</main>
 	</body>
 </html>
