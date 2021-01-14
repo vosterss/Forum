67622d2c0a3d9e44b07ea8d70dca3403121c06e3
LorisTrr <loristerry63430@gmail.com>
2021-01-14 10:30:03 +0100

    login able

--- action_login.php
+++ action_login.php
@@ -1,21 +1,21 @@
 <?php
 $dsn='mysql:host=localhost;dbname=forum';
 $user='root';
-$password='' ;
+$pass='' ;
+$username = htmlspecialchars($_POST['username']);
 try{
-    $bdd = new PDO($dsn, $user, $password);
+    $bdd = new PDO($dsn, $user, $pass);
 }
 catch(PDOException $e){
     die ('erreur :' .$e->getMessage());
 }
-$req = $bdd->prepare('SELECT id, pass FROM utilisateur WHERE username = :username');
+$req = $bdd->prepare('SELECT id, password FROM utilisateur WHERE username = :username');
 $req->execute(array('username' => $username));
 $resultat = $req->fetch();
 
 // Comparaison du pass envoyé via le formulaire avec la base
 $isPasswordCorrect = password_verify($_POST['password'], $resultat['password']);
-
-if (!$resultat)
+if ($resultat == 'false')
 {
     echo 'Mauvais identifiant ou mot de passe !';
 }
