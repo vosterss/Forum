495d5d178ebdb438676f0f9f2b6e70d9f5d6f552
vosterss <77280391+vosterss@users.noreply.github.com>
2021-01-15 16:36:30 +0100

    fe

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 	require 'view/index/header_index.php';
-	//require 'view/index/main_index.php';
-	//require 'view/index/footer_index.php';
+	require 'view/index/main_index.php';
+	require 'view/index/footer_index.php';
 ?>
