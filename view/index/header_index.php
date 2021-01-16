76a0a103bed2b939758b804df94b490bbb1790e8
vosterss <77280391+vosterss@users.noreply.github.com>
2021-01-16 17:06:52 +0100

    fe

--- view/index/header_index.php
+++ view/index/header_index.php
@@ -31,6 +31,29 @@
 						</button>
 					</a>
 				</div>	
-			</div>
+			</div>				
 		</header>
-				
+		<div id="mySidenav" class="sidenav">
+			<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+			<a href="#">Topics</a>
+			<a href="#">Warzone</a>
+			<a href="#">Fortnite</a>
+			<a href="#">Apex Legends</a>
+			<a href="#">PUBG</a>
+		</div>
+		<div id="main">
+			<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
+		</div>		
+		<script>
+			function openNav() {
+			  document.getElementById("mySidenav").style.width = "250px";
+			  document.getElementById("main").style.marginLeft = "250px";
+			  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+			}
+
+			function closeNav() {
+			  document.getElementById("mySidenav").style.width = "0";
+			  document.getElementById("main").style.marginLeft= "0";
+			  document.body.style.backgroundColor = "white";
+			}
+		</script>		
