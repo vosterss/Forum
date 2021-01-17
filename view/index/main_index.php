629f89c3f7a8e27767d8de6888542d09ab6d7d2a
ggiraudet <gael.giraudet@laposte.net>
2021-01-17 21:59:00 +0100

    test grah main et appel post

--- view/index/main_index.php
+++ view/index/main_index.php
@@ -1,6 +1,68 @@
-<main>
-	<h1>Présentation</h1>
-	<div>
-	Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius inventore maiores alias in voluptatum minima, ut blanditiis repudiandae exercitationem enim ratione maxime consequuntur et aliquam voluptates dolorum dicta voluptatibus accusantium?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae quaerat exercitationem error? Provident unde praesentium alias autem eligendi labore id quas commodi animi, eos quo maxime dolore eius laudantium itaque quos molestias mollitia nulla atque obcaecati architecto sapiente. Enim facere eum officiis nobis saepe temporibus voluptates, ea, nemo eligendi non expedita molestiae est natus ipsam quaerat, labore necessitatibus ullam assumenda! Sapiente tempore quod perspiciatis deserunt similique ducimus non ipsam, officiis vel porro beatae dignissimos perferendis quidem quibusdam illo neque eaque vero repellat expedita veniam in aspernatur temporibus cumque? Quod iusto a, temporibus possimus quaerat porro quo facilis tenetur quam ratione?Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quaerat magnam asperiores numquam facilis? Mollitia praesentium consectetur quo rem quidem. Optio eum deserunt amet quaerat doloribus illo quos in nobis minima assumenda voluptatibus ut modi beatae pariatur voluptate iste, placeat eius! Totam, repellendus ipsum. Cum culpa veniam aliquid expedita dolorem odio soluta obcaecati, ipsa dignissimos iure vitae, quos molestias. Quas sunt dolores recusandae doloremque nihil quos tempore consectetur tempora mollitia esse aliquid magnam facilis veritatis voluptatibus, iusto earum quidem voluptates expedita autem ducimus iste omnis? Ducimus voluptate, repellat, eveniet ipsa eos ut eius adipisci obcaecati quidem, rem aspernatur excepturi distinctio at minus harum nemo officia blanditiis modi. Blanditiis atque sit voluptatibus perferendis beatae iusto, quidem, temporibus reprehenderit vel consequatur vitae consequuntur tempore praesentium quo ad nulla dolores aperiam et voluptatum incidunt nesciunt. Odio aut voluptatum laudantium. Cupiditate doloribus quis deserunt animi necessitatibus fugit accusantium iste excepturi, corporis quae. Ab sapiente nemo deleniti error perferendis quos sed, quaerat esse vel maxime ducimus possimus qui saepe nam totam earum architecto ullam officiis enim! Eaque odit quos officiis eos commodi voluptatibus eius quisquam, repellat amet soluta voluptates aperiam ducimus. Pariatur doloremque recusandae fugit natus atque sit optio vel quisquam, ab, illo vero suscipit!
+<main>	
+	<div class='page_acceuil'>
+		<section class="textoverpic">
+		<h1>warzone</h1>
+		<div>
+		<?php
+							$dsn='mysql:host=localhost;dbname=forum';
+							$user='root';
+							$pass='' ;
+							$_SESSION='';
+							try{
+								$bdd = new PDO($dsn, $user, $pass);
+							}
+							catch(PDOException $e){
+								die ('erreur :' .$e->getMessage());
+							}
+							$reponse = $bdd->query('select uti.username, pos.catégorie , pos.id, pos.titre, pos.contenu from utilisateur as uti inner join post as pos on uti.id = pos.id_utilisateur where pos.catégorie=0');
+							$reponse->execute(array('M','24'));
+							$results = $reponse->fetchAll();
+							foreach ($results as $result) {
+								echo $result['username'].' - '.$result['titre'].' - '.$result['contenu'];
+								if($_SESSION!==''){
+									echo '<astyle="padding-left:15px;" href="delete.php?id='.$result['id'].'">delete</a><br>';}
+					}
+					?>
+		</div>
+		</section>
+
+		<section>
+		<h1>fortnite</h1>
+		<div><?php
+					$reponse = $bdd->query('select uti.username, pos.catégorie, pos.id, pos.titre, pos.contenu from utilisateur as uti inner join post as pos on uti.id = pos.id_utilisateur where pos.catégorie=1');
+							$reponse->execute(array('M','24'));
+							$results = $reponse->fetchAll();
+							foreach ($results as $result) {
+								echo $result['username'].' - '.$result['titre'].' - '.$result['contenu'];
+								if($_SESSION!==''){
+									echo '<astyle="padding-left:15px;" href="delete.php?id='.$result['id'].'">delete</a><br>';}}?></div>
+		</section>
+
+		<section class="pop">
+		<h1>pubg</h1>
+		<div><?php
+					$reponse = $bdd->query('select uti.username, pos.catégorie, pos.id, pos.titre, pos.contenu from utilisateur as uti inner join post as pos on uti.id = pos.id_utilisateur where pos.catégorie=2');
+							$reponse->execute(array('M','24'));
+							$results = $reponse->fetchAll();
+							foreach ($results as $result) {
+								echo $result['username'].' - '.$result['titre'].' - '.$result['contenu'];
+								if($_SESSION!==''){
+									echo '<astyle="padding-left:15px;" href="delete.php?id='.$result['id'].'">delete</a><br>';}}?></div>
+		</section>
+
+		<section>
+		<h1>apex</h1>
+		<div><?php
+					$reponse = $bdd->query('select uti.username,pos.catégorie, pos.id, pos.titre, pos.contenu from utilisateur as uti inner join post as pos on uti.id = pos.id_utilisateur where pos.catégorie=3');
+							$reponse->execute(array('M','24'));
+							$results = $reponse->fetchAll();
+							foreach ($results as $result) {
+								echo $result['username'].' - '.$result['titre'].' - '.$result['contenu'].'<br>';
+								if($_SESSION!==''){
+									echo '<astyle="padding-left:15px;" href="delete.php?id='.$result['id'].'">delete</a><br>';}}?></div>
+		</section>
+		<section>
+			
+		</section>
 	</div>
 </main>
