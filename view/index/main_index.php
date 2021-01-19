85476f2ede5c338d70e9ca0f9e98f4e38e7b2176
LorisTrr <loristerry63430@gmail.com>
2021-01-19 11:40:53 +0100

    zdz

--- view/index/main_index.php
+++ view/index/main_index.php
@@ -1,12 +1,16 @@
 <main>	
 	<div class='page_acceuil'>
 		<section class="textoverpic">
-			<h1>warzone</h1>
+			
 			<div>
+				<div class="top">
+					<h1 style="font-size:3em;">Warzone</h1>
+					<h3> Dernier Post</h3>
+				</div>
 				<?php
 					$dsn='mysql:host=localhost;dbname=forum';
 					$user='root';
-					$pass='root' ;
+					$pass='' ;
 					$_SESSION='';
 					try{
 						$bdd = new PDO($dsn, $user, $pass);
@@ -14,13 +18,13 @@
 					catch(PDOException $e){
 						die ('erreur :' .$e->getMessage());
 					}
-					$reponse = $bdd->query('select uti.username, pos.catégorie , pos.id, pos.titre, pos.contenu from utilisateur as uti inner join post as pos on uti.id = pos.id_utilisateur where pos.catégorie=0');
+					$reponse = $bdd->query('select uti.username, pos.catégorie , max(pos.id), pos.titre, pos.contenu from utilisateur as uti inner join post as pos on uti.id = pos.id_utilisateur where pos.catégorie=0');
 					$reponse->execute(array('M','24'));
 					$results = $reponse->fetchAll();
 					foreach ($results as $result) {
-						echo $result['username'].' - '.$result['titre'].' - '.$result['contenu'];
+						echo "<div id='post_index'>"  .$result['titre'].' <br> ' .$result['username'].' <br> '.$result['contenu'];
 						if($_SESSION!==''){
-							echo '<astyle="padding-left:15px;" href="delete.php?id='.$result['id'].'">delete</a><br>';
+							echo '<a style="padding-left:15px;" href="delete.php?id='.$result['id'].'">delete</a><br>';
 						}
 					}
 				?>
@@ -30,13 +34,13 @@
 			<h1>fortnite</h1>
 			<div>
 				<?php
-					$reponse = $bdd->query('select uti.username, pos.catégorie, pos.id, pos.titre, pos.contenu from utilisateur as uti inner join post as pos on uti.id = pos.id_utilisateur where pos.catégorie=1');
+					$reponse = $bdd->query('select uti.username, pos.catégorie, max(pos.id), pos.titre, pos.contenu from utilisateur as uti inner join post as pos on uti.id = pos.id_utilisateur where pos.catégorie=1');
 					$reponse->execute(array('M','24'));
 					$results = $reponse->fetchAll();
 					foreach ($results as $result) {
 						echo $result['username'].' - '.$result['titre'].' - '.$result['contenu'];
 						if($_SESSION!==''){
-							echo '<astyle="padding-left:15px;" href="delete.php?id='.$result['id'].'">delete</a><br>';
+							echo '<a style="padding-left:15px;" href="delete.php?id='.$result['id'].'">delete</a><br>';
 						}
 					}
 				?>	
@@ -46,14 +50,15 @@
 		<section class="pop">
 			<h1>pubg</h1>
 			<div>
+				<h1> Dernier Post</h1>
 				<?php
-					$reponse = $bdd->query('select uti.username, pos.catégorie, pos.id, pos.titre, pos.contenu from utilisateur as uti inner join post as pos on uti.id = pos.id_utilisateur where pos.catégorie=2');
+					$reponse = $bdd->query('select uti.username, pos.catégorie, max(pos.id), pos.titre, pos.contenu from utilisateur as uti inner join post as pos on uti.id = pos.id_utilisateur where pos.catégorie=2');
 					$reponse->execute(array('M','24'));
 					$results = $reponse->fetchAll();
 					foreach ($results as $result) {
 						echo $result['username'].' - '.$result['titre'].' - '.$result['contenu'];
 						if($_SESSION!==''){
-							echo '<astyle="padding-left:15px;" href="delete.php?id='.$result['id'].'">delete</a><br>';
+							echo '<a style="padding-left:15px;" href="delete.php?id='.$result['id'].'">delete</a><br>';
 						}
 					}
 				?>	
@@ -64,13 +69,13 @@
 			<h1>apex</h1>
 			<div>
 				<?php
-					$reponse = $bdd->query('select uti.username,pos.catégorie, pos.id, pos.titre, pos.contenu from utilisateur as uti inner join post as pos on uti.id = pos.id_utilisateur where pos.catégorie=3');
+					$reponse = $bdd->query('select uti.username,pos.catégorie, max(pos.id), pos.titre, pos.contenu from utilisateur as uti inner join post as pos on uti.id = pos.id_utilisateur where pos.catégorie=3');
 					$reponse->execute(array('M','24'));
 					$results = $reponse->fetchAll();
 					foreach ($results as $result) {
 						echo $result['username'].' - '.$result['titre'].' - '.$result['contenu'].'<br>';
 						if($_SESSION!==''){
-							echo '<astyle="padding-left:15px;" href="delete.php?id='.$result['id'].'">delete</a><br>';
+							echo '<a style="padding-left:15px;" href="delete.php?id='.$result['id'].'">delete</a><br>';
 						}
 					}
 				?>	
