895125623bf1d746ccfd2560d3218b4edafeb6a8
vosterss <77280391+vosterss@users.noreply.github.com>
2021-01-22 15:20:23 +0100

    fe

--- view/header_index.php
+++ view/header_index.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 	<head>
 		<meta charset="utf-8" />
-		<link rel="stylesheet" type="text/css" href="CSS/style.css" media="all"/>
+		<link rel="stylesheet" type="text/css" href="../CSS/style.css" media="all"/>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">	
 		<title> Forum </title>
 	</head>
@@ -10,7 +10,7 @@
   		<header>
 		  	<div class="titre">
 				<a href="index.php">
-					<img src="images/dessin.svg" class="logo" alt='image logo'>
+					<img src="../images/dessin.svg" class="logo" alt='image logo'>
 				</a>	
 				<a href="index.php" class="lien-titre">
 					<h1 class="txt-titre"> Forum </h1>
