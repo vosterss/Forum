647875f88601a3a4bed6cf75111f344a2f1b58a2
LorisTrr <loristerry63430@gmail.com>
2021-01-29 13:01:29 +0100

    ;ki

--- models/action_register.php
+++ models/action_register.php
@@ -5,6 +5,7 @@
 	$mail = htmlspecialchars($_POST['mail']);
 	$mdp = sha1(htmlspecialchars($_POST['password'])); 
 	$birthday = htmlspecialchars($_POST['birthday']);
+	
 	$reponse=$bdd->prepare('INSERT INTO utilisateur(surname,name,username,mail,password,birthday) VALUES (?,?,?,?,?,?)') ;
 	$reponse->execute(array($surname,$name,$username,$mail,$mdp,$birthday));
 	echo '<p class="test">Bienvenue, '.$surname. ' '. $name.' vous êtes désormais inscrit.</p>';
