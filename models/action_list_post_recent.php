555f3ca6888cb83736f2fd0573fa7e804130c16f
vosterss <77280391+vosterss@users.noreply.github.com>
2021-01-29 16:43:57 +0100

    fe

--- models/action_list_post_recent.php
+++ models/action_list_post_recent.php
@@ -1,14 +1,5 @@
 <?php
-	function postR (){
-		$reponse = $bdd->prepare('select uti.username, pos.catégorie, max(pos.id), pos.titre, pos.contenu from utilisateur as uti inner join post as pos on uti.id = pos.id_utilisateur where pos.categorie=?');
-		$reponse->execute(array());
-		$result = $reponse->fetchAll();
-	return $result;
-	}
-	##foreach ($results as $result) {
-		##echo $result['username'].' - '.$result['titre'].' - '.$result['contenu'];
-		##if($_SESSION!==''){
-			##echo '<a style="padding-left:15px;" href="delete.php?id='.$result['id'].'">delete</a><br>';
-		##}
-	##}
+	$reponse = $bdd->prepare('select uti.username, pos.catégorie, max(pos.id), pos.titre, pos.contenu from utilisateur as uti inner join post as pos on uti.id = pos.id_utilisateur where pos.categorie');
+	$reponse->execute(array());
+	$results = $reponse->fetchAll();
 ?>
--- view/main_index.php
+++ view/main_index.php
@@ -1,6 +1,15 @@
 	<div class="main-container">
 		<div class="plus-recent">
 			<h1> Post le plus récent </h1>
+			<?php
+				require 'models/action_list_post_recent.php';
+				foreach ($results as $result) {
+					echo $result['username'].' - '.$result['titre'].' - '.$result['contenu'];
+					if($_SESSION!==''){
+					echo '<a style="padding-left:15px;" href="delete.php?id='.$result['id'].'">delete</a><br>';
+					}
+				}
+			?>
 		</div>
 		<div class="plus-like">
 			<h1> Post le plus liké </h1>
