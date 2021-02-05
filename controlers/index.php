5f94a14bb85f32d8ee3c9754771b606d7145d83c
LorisTrr <loristerry63430@gmail.com>
2021-02-05 14:56:45 +0100

    dsfzes

--- controlers/index.php
+++ controlers/index.php
@@ -1,5 +1,7 @@
 <?php
 	require 'view/header_index.php';
+	require 'models/action_post_like.php';
+	$result_L = post_like($bdd);
 	require 'view/main_index.php';
 	require 'view/footer_index.php';
 ?>
