f7d36ef00b5eb26371f757309f24bf59dd10f4db
vosterss <77280391+vosterss@users.noreply.github.com>
2021-02-05 14:56:06 +0100

    fe

--- controlers/index.php
+++ controlers/index.php
@@ -1,5 +1,9 @@
 <?php
 	require 'view/header_index.php';
+	require 'models/action_post_recent.php';
+	require 'models/action_post_like.php';
+	$result = postL()
+	require 'models/action_post_populaire.php';
 	require 'view/main_index.php';
 	require 'view/footer_index.php';
 ?>
