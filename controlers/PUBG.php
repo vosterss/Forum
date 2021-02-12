37852ff9fbeb3cf7a0770aa4742d28cdd3333f7e
LorisTrr <loristerry63430@gmail.com>
2021-02-12 12:13:40 +0100

    fonction op

--- controlers/PUBG.php
+++ controlers/PUBG.php
@@ -2,8 +2,8 @@
 	require 'view/header_index.php';
 	require 'models/function_post.php';
 	$result_L = post_like_categorie($bdd, $categorie);
-	//$result_R = post_recent_categorie($bdd, $categorie);
-	//$result_P = post_populaire_categorie($bdd, $categorie);
+	$result_R = post_recent_categorie($bdd, $categorie);
+	$result_P = post_populaire_categorie($bdd, $categorie);
 	require 'view/main_index.php';
 	require 'view/footer_index.php';
 ?>
