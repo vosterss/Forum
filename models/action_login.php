494c303a68f1e0419be7a11955f40daded69869e
LorisTrr <loristerry63430@gmail.com>
2021-02-19 15:58:56 +0100

    menu deroulant

--- models/action_login.php
+++ models/action_login.php
@@ -1,23 +1,27 @@
 <?php
     $username = htmlspecialchars($_POST['username']);
     $mdp_hash = sha1(htmlspecialchars($_POST['password']));
-    $req = $bdd->prepare('SELECT id, password FROM utilisateur WHERE username = :username');
+    $req = $bdd->prepare('SELECT id, password, ban FROM utilisateur WHERE username = :username');
     $req->execute(array('username' => $username));
     $resultat = $req->fetch();
     if($resultat){
     // Comparaison du pass envoyé via le formulaire avec la base
-        if ($mdp_hash == $resultat['password']){
+        if ($mdp_hash == $resultat['password'] && $resultat['ban'] == 0){
             $_SESSION['id'] = $resultat['id'];
             $_SESSION['username'] = $username;
             header('Location:index.php');
 
         }
+        else if ($resultat['ban'] == 1){
+            // Si la personne est bannie :
+            header('Location:index.php?p=login');
+        }
         else {
-            header('Location:login.php');
+            header('Location:index.php?p=login');
         }
     }
     else{
-        header('Location:login.php');
+        header('Location:index.php?p=login');
     }
 
 ?>
--- view/header_index.php
+++ view/header_index.php
@@ -39,10 +39,20 @@
 				}
 				else{
 			?>
+					<div class="bouton-profil">
+		        		<button class="bouton-L" type="button">		                		
+		                	<h2> Lieux ▼ </h2>
+		        		</button>
+		        		<div class="dropdown-menu-profil">
+							<a href="">Mon profil</a>
+							<a href="">Mes Post</a>
+							<a href="">Déconnexion</a>
+						</div>
+		        	</div>
 					<div class="bouton-theme">		                	
 						<a id="connect" href="index.php?p=deco">         
 							<button class="bouton-L" type="button">
-								<p class="txt-bouton-connection"> Déconnection </p>
+								<p class="txt-bouton-connection"> Déconnexion </p>
 							</button>
 						</a>
 					</div>		
