09b10ecaed6e37a3594fd11cd73ee92db2d512e5
LorisTrr <loristerry63430@gmail.com>
2021-02-23 14:07:59 +0100

    wysiwyg bb

--- view/main_post.php
+++ view/main_post.php
@@ -1,3 +1,8 @@
+		<script>
+			$(function() {
+			  $("#editor").wysibb();
+			})
+		</script>
 
 	<div class='post-container'>
 		<div class="register">
@@ -5,7 +10,9 @@
 				<h1> Création d'un nouveau post</h1>
 			</div>
 			<form action="action_create_post.php" method="post">
+				<textarea id="editor"></textarea>
 				<input class="form_input" type="submit" id='submit' value='Créer'>
+				
 			</form>
 		</div>
 	</div>
