d014bf3b025d8812bd888cf132e03677eb213beb
LorisTrr <loristerry63430@gmail.com>
2021-02-23 14:49:18 +0100

    greger

--- view/main_post.php
+++ view/main_post.php
@@ -5,14 +5,23 @@
 		</script>
 
 	<div class='post-container'>
-		<div class="register">
-			<div class="titre_register">
+		<div class="box-newpost">
+			<div class="newpost-titre">
 				<h1> Création d'un nouveau post</h1>
 			</div>
 			<form action="action_create_post.php" method="post">
+				<input type="text" name="titre_post" placeholder="Titre">
+				<div>
+					<select name="categorie" placeholder="categorie">
+						<option value="">Catégories</option>
+					    <option value="0">Warzone</option>
+					    <option value="1">Fortnite</option>
+					    <option value="2">Apex</option>
+					    <option value="3">PUBG</option>
+					</select>
+				</div>
 				<textarea id="editor"></textarea>
 				<input class="form_input" type="submit" id='submit' value='Créer'>
-				
 			</form>
 		</div>
 	</div>
