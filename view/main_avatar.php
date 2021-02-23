5d99795936187057ed2f20c6832f890b0c3bfc29
vosterss <77280391+vosterss@users.noreply.github.com>
2021-02-23 16:40:11 +0100

    Update main_avatar.php

--- view/main_avatar.php
+++ view/main_avatar.php
@@ -1,4 +1,21 @@
 	<div>
-		<p> avatar </p>
+		<p> Votre avatar </p>
+		<img src='<?php echo $avatar ; ?>' class="avatar" alt=""/>
+		<p> choix avatar </p>
+		<a href="">
+			<img src="images/avatar1.jpg" class="avatar" alt=""/>
+		</a>
+		<a href="">
+			<img src="images/avatar2.jpg" class="avatar" alt=""/>
+		</a>	
+		<a href="">
+			<img src="images/avatar3.jpg" class="avatar" alt=""/>
+		</a>	
+		<a href="">
+			<img src="images/avatar4.jpg" class="avatar" alt=""/>
+		</a>	
+		<a href="">
+			<img src="images/avatar5.jpg" class="avatar" alt=""/>
+		</a>	
 	</div>
 </main>
