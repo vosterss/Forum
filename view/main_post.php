dd6b269a6c0329b3fde48c61eaea48c8d9c42a81
ggiraudet <gael.giraudet@laposte.net>
2021-02-24 20:09:41 +0100

    comm

--- view/main_post.php
+++ view/main_post.php
@@ -12,7 +12,7 @@
 	<h1> Réponse </h1>
 	<div>
 		<?php
-			//var_dump($post);
+			##var_dump($comm);
 			
 			foreach ($comm as $value) {
 				echo $value['username'], $value['message'], $value['date'] .'<br>';
@@ -20,5 +20,9 @@
 			}
 		?>
 	</div>
-
+	<h2> Répondez : </h2>
+	<form action="index.php?p=a_create_comm" method="post">
+		<textarea id="editor" name="contenu" minlength="20" placeholder='votre réponse'></textarea>
+		<input class="form_input" type="submit" id='submit' value='Créer'>
+	</form>
 </main>
