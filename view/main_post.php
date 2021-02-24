ee7dde9f3d3aadc93db61438ad951fdbb4c728e5
ggiraudet <gael.giraudet@laposte.net>
2021-02-24 19:44:56 +0100

    essaie commentair

--- view/main_post.php
+++ view/main_post.php
@@ -1,8 +1,21 @@
+	
+	<h1> message </h2>
 	<div>
 		<?php
 			//var_dump($post);
 			foreach ($post as $value) {
-				echo $value['username'], $value['titre'],$value['contenu'],$value['date_publication'];
+				echo $value['username'], $value['titre'],$value['contenu'],$value['date_publication'].'<br>';
+
+			}
+		?>
+	</div>
+	<h1> Réponse </h1>
+	<div>
+		<?php
+			//var_dump($post);
+			
+			foreach ($comm as $value) {
+				echo $value['username'], $value['message'], $value['date'] .'<br>';
 
 			}
 		?>
