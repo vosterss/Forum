535270abd93b632a75ef63c3d5ccb8d966c23a5a
LorisTrr <loristerry63430@gmail.com>
2021-02-26 08:05:39 +0100

    bdd

--- forum(1).sql
+++ forum(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 24 fév. 2021 à 10:52
+-- Généré le : ven. 26 fév. 2021 à 08:04
 -- Version du serveur :  10.4.17-MariaDB
 -- Version de PHP : 8.0.2
 
@@ -42,23 +42,34 @@ CREATE TABLE `message` (
   `id` bigint(20) NOT NULL,
   `id_post` bigint(20) NOT NULL,
   `message` varchar(1000) NOT NULL,
-  `id_user` bigint(20) NOT NULL
+  `id_utilisateur` bigint(20) NOT NULL,
+  `date_reponse` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `message`
 --
 
-INSERT INTO `message` (`id`, `id_post`, `message`, `id_user`) VALUES
-(1, 1, 'voici mon message ', 10),
-(2, 2, 'dsqdflksqn,nfd,n  dks,qkdkjQLKSJDLKQSJLDKJ SKQDLKJlksjdlklqsk', 4),
-(3, 5, 'SQDqsdsqDSQdsqDSQDSQdsqDSQDQSdsqdsqdqsdqsdqsdqsdqDQSDqdssdsqdsqdqSDDSQdqsDQSDQDdsqdQDqsdqsdqsdsqDD', 5),
-(4, 2, 'SQDqsdsqDSQdsqDSQDSQdsqDSQDQSdsqdsqdqsdqsdqsdqsdqDQSDqdssdsqdsqdqSDDSQdqsDQSDQDdsqdQDqsdqsdqsdsqDD', 1),
-(5, 1, 'SQDqsdsqDSQdsqDSQDSQdsqDSQDQSdsqdsqdqsdqsdqsdqsdqDQSDqdssdsqdsqdqSDDSQdqsDQSDQDdsqdQDqsdqsdqsdsqDD', 3),
-(6, 2, 'dsqdflksqn,nfd,n  dks,qkdkjQLKSJDLKQSJLDKJ SKQDLKJlksjdlklqsk', 4),
-(7, 5, 'SQDqsdsqDSQdsqDSQDSQdsqDSQDQSdsqdsqdqsdqsdqsdqsdqDQSDqdssdsqdsqdqSDDSQdqsDQSDQDdsqdQDqsdqsdqsdsqDD', 5),
-(8, 2, 'SQDqsdsqDSQdsqDSQDSQdsqDSQDQSdsqdsqdqsdqsdqsdqsdqDQSDqdssdsqdsqdqSDDSQdqsDQSDQDdsqdQDqsdqsdqsdsqDD', 1),
-(9, 1, 'SQDqsdsqDSQdsqDSQDSQdsqDSQDQSdsqdsqdqsdqsdqsdqsdqDQSDqdssdsqdsqdqSDDSQdqsDQSDQDdsqdQDqsdqsdqsdsqDD', 3);
+INSERT INTO `message` (`id`, `id_post`, `message`, `id_utilisateur`, `date_reponse`) VALUES
+(1, 1, 'voici mon message ', 10, '2021-02-24 20:26:42'),
+(2, 2, 'dsqdflksqn,nfd,n  dks,qkdkjQLKSJDLKQSJLDKJ SKQDLKJlksjdlklqsk', 4, '2021-02-24 20:26:42'),
+(3, 5, 'SQDqsdsqDSQdsqDSQDSQdsqDSQDQSdsqdsqdqsdqsdqsdqsdqDQSDqdssdsqdsqdqSDDSQdqsDQSDQDdsqdQDqsdqsdqsdsqDD', 5, '2021-02-24 20:26:42'),
+(4, 2, 'SQDqsdsqDSQdsqDSQDSQdsqDSQDQSdsqdsqdqsdqsdqsdqsdqDQSDqdssdsqdsqdqSDDSQdqsDQSDQDdsqdQDqsdqsdqsdsqDD', 1, '2021-02-24 20:26:42'),
+(5, 1, 'SQDqsdsqDSQdsqDSQDSQdsqDSQDQSdsqdsqdqsdqsdqsdqsdqDQSDqdssdsqdsqdqSDDSQdqsDQSDQDdsqdQDqsdqsdqsdsqDD', 3, '2021-02-24 20:26:42'),
+(6, 2, 'dsqdflksqn,nfd,n  dks,qkdkjQLKSJDLKQSJLDKJ SKQDLKJlksjdlklqsk', 4, '2021-02-24 20:26:42'),
+(7, 5, 'SQDqsdsqDSQdsqDSQDSQdsqDSQDQSdsqdsqdqsdqsdqsdqsdqDQSDqdssdsqdsqdqSDDSQdqsDQSDQDdsqdQDqsdqsdqsdsqDD', 5, '2021-02-24 20:26:42'),
+(8, 2, 'SQDqsdsqDSQdsqDSQDSQdsqDSQDQSdsqdsqdqsdqsdqsdqsdqDQSDqdssdsqdsqdqSDDSQdqsDQSDQDdsqdQDqsdqsdqsdsqDD', 1, '2021-02-24 20:26:42'),
+(9, 1, 'SQDqsdsqDSQdsqDSQDSQdsqDSQDQSdsqdsqdqsdqsdqsdqsdqDQSDqdssdsqdsqdqSDDSQdqsDQSDQDdsqdQDqsdqsdqsdsqDD', 3, '2021-02-24 20:26:42'),
+(10, 2, 'JE SUIS BIEN L4AUTEUR DE CETTE R2PONSE', 13, '2021-02-24 20:39:15'),
+(11, 2, 'je trouve se post pas tres pertinent lol', 13, '2021-02-24 20:44:05'),
+(12, 2, '', 14, '2021-02-24 21:04:33'),
+(13, 2, '', 14, '2021-02-24 21:06:07'),
+(14, 2, 'htrzhzthzthtrvfqgfdg', 14, '2021-02-24 21:07:38'),
+(15, 2, 'Cette fois cela va marcher, enfin jespere ahah', 14, '2021-02-24 21:15:26'),
+(16, 2, 'gedbgrengjrengrargagraaaa', 14, '2021-02-25 08:10:35'),
+(17, 2, '', 14, '2021-02-25 08:13:44'),
+(18, 1, 'htrrzhtjtrjztzjrrjzthztrzh', 14, '2021-02-25 09:20:37'),
+(19, 1, 'rhehrheherherhrehrebhrthtr', 14, '2021-02-25 09:21:54');
 
 -- --------------------------------------------------------
 
@@ -76,11 +87,11 @@ CREATE TABLE `pht_profil` (
 --
 
 INSERT INTO `pht_profil` (`id`, `avatar`) VALUES
-(1, 'images/avatar1.jpg'),
-(2, 'images/avatar2.jpg'),
-(3, 'images/avatar3.jpg'),
-(4, 'images/avatar4.jpg'),
-(5, 'images/avatar5.jpg');
+(0, 'images/avatar1.jpg'),
+(1, 'images/avatar2.jpg'),
+(2, 'images/avatar3.jpg'),
+(3, 'images/avatar4.jpg'),
+(4, 'images/avatar5.jpg');
 
 -- --------------------------------------------------------
 
@@ -94,7 +105,7 @@ CREATE TABLE `post` (
   `contenu` longtext NOT NULL,
   `id_utilisateur` bigint(20) NOT NULL,
   `date_publication` timestamp NOT NULL DEFAULT current_timestamp(),
-  `post_fermer_ouvert` int(1) NOT NULL,
+  `isclosed` int(1) NOT NULL,
   `categorie` int(4) NOT NULL,
   `nb_like` int(255) DEFAULT NULL,
   `nb_mess` int(255) NOT NULL
@@ -104,9 +115,10 @@ CREATE TABLE `post` (
 -- Déchargement des données de la table `post`
 --
 
-INSERT INTO `post` (`id`, `titre`, `contenu`, `id_utilisateur`, `date_publication`, `post_fermer_ouvert`, `categorie`, `nb_like`, `nb_mess`) VALUES
+INSERT INTO `post` (`id`, `titre`, `contenu`, `id_utilisateur`, `date_publication`, `isclosed`, `categorie`, `nb_like`, `nb_mess`) VALUES
 (1, 'Je partage ici la classe Warzone la plus cheater ', 'la MAC 10 !!!!!!!!!!!!!!!', 13, '2024-02-21 08:31:04', 0, 0, NULL, 0),
-(2, 'edgvagreaga', 'garegreagreagaregaer', 13, '2024-02-21 09:34:07', 0, 0, NULL, 0);
+(2, 'edgvagreaga', 'garegreagreagaregaer', 13, '2024-02-21 09:34:07', 0, 0, NULL, 0),
+(3, '(yg\'arhétrh', 'htzhthzrthrtzhtrzhzr', 13, '2024-02-21 03:10:31', 0, 0, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -122,20 +134,22 @@ CREATE TABLE `utilisateur` (
   `mail` varchar(150) NOT NULL,
   `password` varchar(100) NOT NULL,
   `birthday` date NOT NULL,
-  `id_avatar` int(11) NOT NULL,
-  `ban` int(11) NOT NULL
+  `avatar` text NOT NULL,
+  `ban` int(11) NOT NULL,
+  `id_droit` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`id`, `surname`, `name`, `username`, `mail`, `password`, `birthday`, `id_avatar`, `ban`) VALUES
-(10, 'atmo', 'atmokk', 'assos1', 'gael.giraudet@laposte.net', 'f7c3bc1d808e04732adf679965ccc34ca7agezgzegze', '2021-01-26', 0, 0),
-(11, 'lkklk', 'hyghghgh', 'hjhjh', 'gaael.giraudet@laposte.net', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', '2021-01-21', 0, 0),
-(12, 'admin', 'admin', 'giraudet', 'gael.giraudet@laposte.net', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', '2031-01-19', 0, 0),
-(13, 'Loris', 'Terry', 'LorisTrr', 'loristerry63430@gmail.com', 'a8d2436eb1e79db3aabb4d46e814d4f34436cb03', '2001-08-09', 1, 0),
-(14, 'Thomas', 'Veira', 'Vosters', 'thomasvieira@gmail.com', '5a10593e262f95659897db18592f1bcfe3aa0bc9', '2001-08-09', 1, 0);
+INSERT INTO `utilisateur` (`id`, `surname`, `name`, `username`, `mail`, `password`, `birthday`, `avatar`, `ban`, `id_droit`) VALUES
+(10, 'atmo', 'atmokk', 'assos1', 'gael.giraudet@laposte.net', 'f7c3bc1d808e04732adf679965ccc34ca7agezgzegze', '2021-01-26', 'images/avatar1.jpg', 0, 0),
+(11, 'lkklk', 'hyghghgh', 'hjhjh', 'gaael.giraudet@laposte.net', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', '2021-01-21', 'images/avatar1.jpg', 0, 0),
+(12, 'admin', 'admin', 'giraudet', 'gael.giraudet@laposte.net', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', '2031-01-19', 'images/avatar1.jpg', 0, 0),
+(13, 'Loris', 'Terry', 'LorisTrr', 'loristerry63430@gmail.com', 'a8d2436eb1e79db3aabb4d46e814d4f34436cb03', '2001-08-09', 'images/avatar4.jpg', 0, 2),
+(14, 'Thomas', 'Veira', 'Vosters', 'thomasvieira@gmail.com', '5a10593e262f95659897db18592f1bcfe3aa0bc9', '2001-08-09', 'images/avatar1.jpg', 0, 2),
+(15, 'Ken', 'Kanecki', 'kenkaneck', 'minefragpe@gmail.com', '85e124a2a62b64553de5bf4b49695a85fab92f9c', '2001-08-09', 'images/avatar1.jpg', 0, 0);
 
 --
 -- Index pour les tables déchargées
@@ -147,7 +161,7 @@ INSERT INTO `utilisateur` (`id`, `surname`, `name`, `username`, `mail`, `passwor
 ALTER TABLE `message`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_post` (`id_post`),
-  ADD KEY `id_user` (`id_user`);
+  ADD KEY `id_user` (`id_utilisateur`);
 
 --
 -- Index pour la table `pht_profil`
@@ -176,7 +190,7 @@ ALTER TABLE `utilisateur`
 -- AUTO_INCREMENT pour la table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT pour la table `pht_profil`
@@ -188,13 +202,13 @@ ALTER TABLE `pht_profil`
 -- AUTO_INCREMENT pour la table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
