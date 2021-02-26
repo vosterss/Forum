2ec25e12f74266eb4be7981cc1b487df1ad95287
ggiraudet <gael.giraudet@laposte.net>
2021-02-26 13:48:12 +0100

    debanne

--- view/main_all_user.php
+++ view/main_all_user.php
@@ -6,9 +6,11 @@
 							echo $result['id'].' - '.$result['surname']. ' - ' .$result['name']. ' - ' .$result['username']. ' - ' .$result['mail'];
                             if($result['id_droit'] == 1){
                                 echo 'utilisateur banni';
+                                echo '<a style="padding-left:15px;" href=index.php?p=debannir&id='.$result['id'].'>débannir</a><br>';
                             }
 							echo '<a style="padding-left:15px;" href=index.php?p=delete&id='.$result['id'].'>delete admin</a><br>';
-                            echo '<a style="padding-left:15px;" href=index.php?p=bannir&id='.$result['id'].'>bannir</a><br>';
+                            if($result['id_droit'] != 1){
+                            echo '<a style="padding-left:15px;" href=index.php?p=bannir&id='.$result['id'].'>bannir</a><br>';}
                             	
 						}
 					?>
