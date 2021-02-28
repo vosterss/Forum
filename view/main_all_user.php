30bc76af2b2463d8cb9e659bbdf97807204edb57
ggiraudet <gael.giraudet@laposte.net>
2021-02-28 13:41:04 +0100

    all user propre

--- view/main_all_user.php
+++ view/main_all_user.php
@@ -1,10 +1,11 @@
 <div class="main-cont">
+    <div class='cont'>
 					<h1> Les utilisateurs : </h1>
 					<?php
 						foreach ($result_m as $result){
 							
                             echo 
-								"<div class='plus-like'>
+								"<div class='user'>
                                 <div class='plus-like-container'>
                                 <div class='auteur'>
 									<div class='utilisateur'>
@@ -26,16 +27,20 @@
                             if($result['id_droit'] == 1){
                                 echo ' utilisateur banni ';
                                 echo '<a style="padding-left:15px;" href=index.php?p=debannir&id='.$result['id'].'>débannir</a><br>';
+                                echo '</div>';
+                                echo '</div>';
                                
                                 
                             }else if($result['id_droit'] == 3){
                                 echo ' utilisateur banni définitivement ';
                                 echo '<a style="padding-left:15px;" href=index.php?p=debannir&id='.$result['id'].'>débannir</a><br>';
+                                echo '</div>';
+                                echo '</div>';
                                
                                 
                             }
 							
-                            if($result['id_droit'] != 1){
+                            else if($result['id_droit'] != 1 || $result['id_droit'] != 3 ){
                             //echo '<a style="padding-left:15px;" href=index.php?p=bannir&id='.$result['id'].'>bannir</a><br>';
                             echo '<form action="index.php?p=bannir&id='.$result['id'].'" method="POST">
                             <p> Date de déban<input  type="date" name="date_ban" ></p>
@@ -49,5 +54,6 @@
                            
 						}
 					?>
+        </div>
 </div>
 </main>
