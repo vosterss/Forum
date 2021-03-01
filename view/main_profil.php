094c5cb361b8070d6549efcc79ee9afe75087b9a
vosterss <77280391+vosterss@users.noreply.github.com>
2021-03-01 23:29:49 +0100

    fe

--- view/main_profil.php
+++ view/main_profil.php
@@ -1,27 +1,22 @@
 	<div class="main-profil">
-		
 		<div class="avatar-mdp">
-			<h2><p> <?php echo $username ;?><p></h2>
-    			
-    				
-    		
+			<h2><p> <?php echo $username ;?></p></h2>
+			<div class="profil-container">
 				<div class="profil-avatar">
-				<a href="index.php?p=avatar">
-					<img src='<?php echo $avatar ; ?>' class="profil-avatar-img" alt=""/>
-				</a>
-				<a href="index.php?p=avatar">
-					<p> Changer d'avatar </p>
-				</a>
+					<a href="index.php?p=avatar">
+						<img src='<?php echo $avatar ; ?>' class="profil-avatar-img" alt=""/>
+					</a>
+					<a href="index.php?p=avatar">
+						<p> Changer d'avatar </p>
+					</a>
+				</div>
+				<div class='profil-mdp'>Nom : <?php echo $_SESSION['surname']; ?></div>
+				<div class='profil-mdp'>Prénom : <?php echo $_SESSION['name']; ?></div>
+				<div class='profil-mdp'>mail : <?php echo$_SESSION['mail']; ?></div>
 			</div>
-			<div class='profil-mdp'>Nom : <?php echo $_SESSION['surname']; ?></div>
-		<div class='profil-mdp'>Prénom : <?php echo $_SESSION['name']; ?></div>
-		<div class='profil-mdp'>mail : <?php echo$_SESSION['mail']; ?></div>
-			<div class="profil-mdp">
+			<div class="profil-mdp2">
 				<a href="index.php?p=change-mdp">Changer votre mot de passe</a>
 			</div>
-		</div>
-		<aside> 
-	
-		</aside>
+		</div>	
 	<div>	
 </main>
