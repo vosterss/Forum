5fa80dfb9a913ae599a24175aac9bb6966d6db06
vosterss <77280391+vosterss@users.noreply.github.com>
2021-03-01 09:33:52 +0100

    fe

--- view/main_profil.php
+++ view/main_profil.php
@@ -1,7 +1,11 @@
-	<div class="profil-avatar">
-		<a href="index.php?p=avatar">Avatar</a>
-	</div>	
-	<div class="profil-mdp">
-		<a href="index.php?p=change-mdp">Changer votre mot de passe</a>
-	</div>
+	<div class="main-profil">
+		<div class="avatar-mdp">
+			<h2>Profil</h2>
+			<div class="profil-avatar">
+				<a href="index.php?p=avatar">Avatar</a>
+			</div>	
+			<div class="profil-mdp">
+				<a href="index.php?p=change-mdp">Changer votre mot de passe</a>
+			</div>
+	<div>	
 </main>
