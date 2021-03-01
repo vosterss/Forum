6dd8f9aebaa331613bf73299602b44864865b147
LorisTrr <loristerry63430@gmail.com>
2021-03-01 22:54:41 +0100

    fe

--- models/action_create_post.php
+++ models/action_create_post.php
@@ -4,9 +4,8 @@
 	$contenu = htmlspecialchars($_POST['contenu']);
 	//$contenu = $contenu->getAsHtml();
 	//echo $contenu;
-	$date = date('d-m-y h:i:s');
-	$reponse=$bdd->prepare('INSERT INTO post(titre,id_utilisateur,categorie, contenu,date_publication) VALUES (?,?,?,?,?)') ;
-	$reponse->execute(array($titre,$id_utilisateur,$categorie,$contenu,$date));
+	$reponse=$bdd->prepare('INSERT INTO post(titre,id_utilisateur,categorie, contenu) VALUES (?,?,?,?)') ;
+	$reponse->execute(array($titre,$id_utilisateur,$categorie,$contenu));
 	echo '<p class="test">Votre Post : '.$titre. ' a été publié.</p>';
 	header("Refresh:3; url=index.php");
 
